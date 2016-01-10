a7c673552c88567dacc9203917ef8cb71f69c498
Rene Schmidt <rene@reneschmidt.de>
2016-01-10 14:49:35 +0100

    minor fixes

--- test/unit/TestCase.php
+++ test/unit/TestCase.php
@@ -22,13 +22,13 @@ class TestCase extends \PHPUnit_Framework_TestCase
     /**
      * Call protected/private method of a class.
      *
-     * @param object &$object    Instantiated object that we will run method on.
+     * @param object $object     Instantiated object that we will run method on.
      * @param string $methodName Method name to call
      * @param array  $parameters Array of parameters to pass into method.
      *
      * @return mixed Method return.
      */
-    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
     {
         $reflection = new \ReflectionClass(get_class($object));
         $method = $reflection->getMethod($methodName);
